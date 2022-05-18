bf7aadccd2f92c8115c22d2362a305c97f2fcbf8
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-05-18 12:23:40 +0430

    database config file

--- package/config/app.go
+++ package/config/app.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func connect() {
+	d, err := gorm.Open("mysql", "arsalan:arsalan/bookstore?charset=utf8&parseTime=true&loc=Local")
+	if err != nil {
+		fmt.Println("connection to database fails.")
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
